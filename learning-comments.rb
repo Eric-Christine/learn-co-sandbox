6e229f81e5f73050e2bfa18ef1aa6bdc3e75c383
Eric Christine <ericjchristine@gmail.com>
2021-05-15 04:20:19 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,15 @@
+#Perform a constant expression evaluation
+3
+#assign constant 3 to bare-word vairable traingle_sides
+`triangle_sides`
+
+# From the Three Dog Night song: "Joy to the World (Jeremiah was a Bullfrog)"
+puts "Joy to the world"
+puts "All the boys and girls"
+puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
+
+puts "Joy to the world"
+puts "All the boys and girls"
+# puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
